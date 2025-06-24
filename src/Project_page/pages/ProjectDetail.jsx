2ac9c81371bc674450cfb3890a806a80ba220708
Methexx <Methum@1234>
2025-06-24 16:13:03 +0530

    feat(project details)back button enhanced

--- src/Project_page/pages/ProjectDetail.jsx
+++ src/Project_page/pages/ProjectDetail.jsx
@@ -160,13 +160,11 @@ const ProjectDetail = () => {
 
         {/* Bottom Back Button */}
         <div className="bottom-back-container">
-          <button onClick={handleBack} className="bottom-back-button">
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
-            </svg>
-            Back
+          <button onClick={handleBack} className="text-back-button">
+            Back to Previous
           </button>
         </div>
+
       </div>
 
       <Footer />
